dd7220dd218d8bd0ce0e39f1d29007f6a622ff8e
Valentin Hilbig <webmaster@scylla-charybdis.com>
2004-07-23 19:17:34 +0000

    cosmetic changes

--- dbm.c
+++ dbm.c
@@ -1,13 +1,29 @@
 /* $Header$
  *
- * Copyright (C)2004 by Valentin Hilbig
- *
  * A little general DBM wrapper
- *
  * This source shall be independent of others.  Therefor no tinolib.
  *
+ * Copyright (C)2004 by Valentin Hilbig
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
  * $Log$
- * Revision 1.2  2004-07-23 18:48:25  tino
+ * Revision 1.3  2004-07-23 19:17:34  tino
+ * cosmetic changes
+ *
+ * Revision 1.2  2004/07/23 18:48:25  tino
  * minor changes, diagnostic dump and batch key insert/replace added
  *
  * Revision 1.1  2004/07/21 20:07:34  tino
